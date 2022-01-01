e23241fe83bb026f29d0927cfafce63c70495c10
Chris <cchen842@uwo.ca>
2021-12-31 20:22:44 -0500

    initial images

--- blog/src/topbar/Topbar.jsx
+++ blog/src/topbar/Topbar.jsx
@@ -4,12 +4,12 @@ export default function Topbar() {
     return (
         <div className='top'>
             <div className="topLeft">
-                <i className= "topIcon fab fa-facebook-square"></i>
-                <i className= "topIcon fab fa-twitter-square"></i>
-                <i className= "topIcon fab fa-pinterest-square"></i>
-                <i className= "topIcon fab fa-instagram-square"></i>
+                <i className= "icon fab fa-facebook-square"></i>
+                <i className= "icon fab fa-twitter-square"></i>
+                <i className= "icon fab fa-pinterest-square"></i>
+                <i className= "icon fab fa-instagram-square"></i>
             </div>
-            <div className="topCenter">
+            <div className="topMiddle">
                 <ul className="topList">
                     <li className='topListItem'>HOME</li>
                     <li className='topListItem'>ABOUT</li>
@@ -19,7 +19,10 @@ export default function Topbar() {
                 </ul>
             </div>
             <div className="topRight">
-                <img src="" alt="" />
+                <img className='topImage'
+                    src="static/model.png" 
+                    alt="" 
+                />
                 <i className="topSearchIcon fas fa-search"></i>
             </div>
             
