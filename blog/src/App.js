ae1b08cec9b0e914fde88481b336b99160d2ed4d
Chris <cchen842@uwo.ca>
2022-01-01 23:55:47 -0500

    finsihed login page

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,12 +3,13 @@ import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import LoginPage from "./pages/loginPage/LoginPage";
 
 function App() {
   return (
     <>
       <Topbar/>
-      <Settings/>
+      <LoginPage/>
     </>
   );
 }
--- blog/src/pages/loginPage/LoginPage.jsx
+++ blog/src/pages/loginPage/LoginPage.jsx
@@ -3,7 +3,15 @@ import "./loginPage.css"
 export default function LoginPage() {
     return (
         <div className='loginPage'>
-            
+            <span className="loginTitle">Login</span>
+            <form className="loginForm">
+                <label>Email</label>
+                <input type="text" className="loginInput" placeholder="Enter your Email"/>
+                <label>Password</label>
+                <input type="password" className="loginInput" placeholder="Enter your password"/>
+                <button className="loginButton">Login</button>
+            </form>
+            <button className="registerButton">Register</button>
         </div>
     )
 }
