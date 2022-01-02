532372eb54361bb7fed7402f4c612be7a3f2c398
Chris <cchen842@uwo.ca>
2022-01-01 22:32:43 -0500

    added settings folder

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,12 +2,13 @@ import HomePage from "./pages/homePage/HomePage";
 import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 function App() {
   return (
     <>
       <Topbar/>
-      <Write/>
+      <Settings/>
     </>
   );
 }
