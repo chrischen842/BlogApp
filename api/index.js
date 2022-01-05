95ac89affa7f8a682d2809b8d5b22c256c1e5dae
Chris <cchen842@uwo.ca>
2022-01-05 13:23:27 -0500

    fixed connection error

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(console.log("connection established")).catch((err) => console.log(err));
 
 app.listen("80", () => {
     console.log("I am running")
