11fdd923d9c4c1cdbb78e3309dad13e91822b082
Chris <cchen842@uwo.ca>
2022-01-05 22:06:45 -0500

    finished categories

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -17,9 +19,23 @@ mongoose
     .then(console.log("connection established"))
     .catch((err) => console.log(err));
 
+const stroage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    }, filename: (req, file, cb) => {
+        cb(null, "lofi.jpg");
+    },     
+});
+
+const upload = multer({storage:stroage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded")
+})
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("80", () => {
     console.log("I am running");
